95112dde92960fa9d68bf47c55d85c07e45b3105
borsics <borscsa1@gmail.com>
2018-09-25 08:40:14 +0200

    Scan->Add File not Found

--- src/EngineHandler.cpp
+++ src/EngineHandler.cpp
@@ -11,7 +11,11 @@ void EngineHandler::scan(const QString &path)
 			utils.qStdOut() << jsonHelper.createJSON().toJson(QJsonDocument::Indented);
 			break;
 		case static_cast<int>(utils::Verdict ::Threat):
-
+			jsonHelper.clearJSON();
+			jsonHelper.addToJSON("file_name", path);
+			jsonHelper.addToJSON("verdict", "blocked");
+			jsonHelper.createNode();
+			utils.qStdOut() << jsonHelper.createJSON().toJson(QJsonDocument::Indented);
 			break;
 		case static_cast<int>(utils::Verdict ::Error):
 
