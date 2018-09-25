a6dc1ee486deb42f4de3d9391b5c2fcc2e0eb02d
borsics <borscsa1@gmail.com>
2018-09-25 08:38:54 +0200

    Scan->Add the Block

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -2,3 +2,45 @@
 #include "Utils.h"
 
 #include "iostream"
+bool Engine::init()
+{
+	return database.connectToDatabase();
+}
+bool Engine::lookup( QString hash )
+{
+	return database.findInDatabase(hash);
+}
+qint16 Engine::fileScan( QString path )
+{
+	if (!QFileInfo::exists(path)) {
+		return static_cast<qint16>(utils::Verdict::Error);
+	}
+
+	if (database.findInDatabase(hashes(path)["md5"], hashes(path)["sha1"], hashes(path)["sha256"])) {
+		return static_cast<qint16>(utils::Verdict::Threat);
+	}
+	else {
+		return static_cast<qint16>(utils::Verdict::Clear);
+	}
+}
+QMap<QString, QString> Engine::hashes( QString path )
+{
+	QMap<QString, QString> qmap;
+	qmap["md5"] = fileHashGenerate(path, QCryptographicHash::Md5);
+	qmap["sha1"] = fileHashGenerate(path, QCryptographicHash::Sha1);
+	qmap["sha256"] = fileHashGenerate(path, QCryptographicHash::Sha256);
+
+	return qmap;
+}
+QString Engine::fileHashGenerate( QString path, QCryptographicHash::Algorithm hashAlgoritm )
+{
+	QFile file(path);
+	if (file.open(QFile::ReadOnly)) {
+		QCryptographicHash hash(hashAlgoritm);
+		if (hash.addData(&file)) {
+			return hash.result().toHex();
+		}
+	}
+
+	return QString();
+}
--- src/EngineHandler.cpp
+++ src/EngineHandler.cpp
@@ -1,2 +1,23 @@
 #include "EngineHandler.h"
 
+void EngineHandler::scan(const QString &path)
+{
+	switch (engine.fileScan(path)) {
+		case static_cast<int>(utils::Verdict ::Clear):
+			jsonHelper.clearJSON();
+			jsonHelper.addToJSON("file_name", path);
+			jsonHelper.addToJSON("verdict", "No threat detected");
+			jsonHelper.createNode();
+			utils.qStdOut() << jsonHelper.createJSON().toJson(QJsonDocument::Indented);
+			break;
+		case static_cast<int>(utils::Verdict ::Threat):
+
+			break;
+		case static_cast<int>(utils::Verdict ::Error):
+
+			break;
+		default:
+			break;
+	}
+}
+
